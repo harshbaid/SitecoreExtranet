097ef17329cb137b800361cfba90f432b408bdcb
Mark Stiles <stiles.mark@gmail.com>
2014-05-25 11:13:48 -0400

    *moved shared functionality to the base page

--- Sitecore.Extranet.Core/Wizards/BasePage.cs
+++ Sitecore.Extranet.Core/Wizards/BasePage.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Sitecore.Web.UI.XmlControls;
 using Sitecore.Web.UI.Pages;
+using Sitecore.Data;
 
 namespace Sitecore.Extranet.Core.Wizards {
 	/// <summary>
@@ -12,9 +13,33 @@ namespace Sitecore.Extranet.Core.Wizards {
 	/// </summary>
 	public class BasePage : WizardDialogBaseXmlControl {
 
+		private Database _mdb;
+		public Database MasterDB {
+			get {
+				if (_mdb == null)
+					_mdb = Sitecore.Configuration.Factory.GetDatabase("master");
+				return _mdb;
+			}
+		}
+
 		public string PageName { get; set; }
 
 		public virtual bool IsValid { get { return true; } }
+
+		protected override void OnLoad(EventArgs e) {
+
+			// similar to is not PostBack.
+			if (!Sitecore.Context.ClientPage.IsEvent) {
+				InitializeControl();
+			}
+			base.OnLoad(e);
+		}
+
+		protected virtual void InitializeControl(){ }
+		
+		protected string SummaryStr(string name, string value) {
+			return string.Format("{0}: <span class='value'>{1}</span>", name, value);
+		}
 
 		public virtual IEnumerable<string> DataSummary {
 			get {
